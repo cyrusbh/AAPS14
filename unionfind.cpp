e15ff88bb0aeefc367178289d092b18995b2cefc
cyrusbh <markus.niemela@gmail.com>
2014-02-02 18:37:36 +0100

    Union-Find Accepted
    Union-Find Disjoint Set data structure

--- unionfind.cpp
+++ unionfind.cpp
@@ -2,6 +2,9 @@
 * Author:   Markus Niemelä
 * Problem:  Union-Find
 * Started:  2014-02-02
+*
+* Submission 2: ints substituted for longs in certain places.
+* Submission 3: Changed find(long) function to return root() instead of r.
 */
 
 #include <cstdio>
@@ -13,49 +16,43 @@ struct UFNode {
     UFNode* r;
 	int rank;
 	
-	UFNode(){
-		r = this;
-		rank = 0;
-	}
-	
     UFNode* root(){
-		printf("X\n");
         if(r != this)
-			printf("Y\n");
             r = r->root();
         return r;
     }
     
     void join(UFNode* v){
-		printf("A\n");
         if(root() != v->root()){
-			printf("B\n");
 			if(r->rank < v->r->rank){
 				r->r = v->r;
-				printf("C\n");
 			}
 			else if(r->rank > v->r->rank){
-				printf("D\n");
 				v->r->r = r;
 			}
 			else{
 				v->r->r = r;
 				r->rank++;
-				printf("E\n");
 			}
 		}
     }
 };
 
 class UnionFindSet{
-		vector<UFNode> sets;
+		vector<UFNode*> sets;
 	public:
-		UnionFindSet(long n) : sets(n){};
+		UnionFindSet(long n) : sets(n){
+			for(long i = 0; i < n; i++){
+				sets[i] = new UFNode();
+				sets[i]->r = sets[i];
+				sets[i]->rank = 0;
+			}
+		};
 		void join(long a, long b){
-			sets[a].join(&sets[b]);
+			sets[a]->join(sets[b]);
 		}
 		UFNode* find(long a){
-			return sets[a].r;
+			return sets[a]->root();
 		}
 };
 
@@ -65,12 +62,10 @@ int main(int argc, char* argv[]){
 	scanf("%d %d", &n, &q);
 	UnionFindSet uf(n);
 	
-	for(int i = 0; i < q; i++){
+	for(long i = 0; i < q; i++){
 		scanf("\n%c %d %d", &c, &a, &b);
-		if(c == '='){
+		if(c == '=')
 			uf.join(a, b);
-			printf("Joined %c and %b\n", a, b);
-		}
 		else
 			printf("%s\n", uf.find(a) == uf.find(b) ? "yes" : "no");
 	}
