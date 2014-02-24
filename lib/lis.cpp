c2dc09ea96c063138d31d08e50ef93f1b3b700e6
cyrusbh <markus.niemela@gmail.com>
2014-02-24 15:41:52 +0100

    Longest Increasing Subsequence Accepted
    Accepted, also added as library function.

--- lib/lis.cpp
+++ lib/lis.cpp
@@ -1,24 +1,35 @@
 /**
+* Filename: lis.cpp
 * Author:   Markus Niemelä
-* Problem:  Longest Increasing Subsequence
-* Started:  2014-02-23
 */
 
-#include <cstdio>
-#include <vector>
-#include <algorithm>
+#include "lis.h"
+
+// Longest increasing subsequence. Implementation of algorithm found at 
+// http://en.wikipedia.org/wiki/Longest_increasing_subsequence#Efficient_algorithms 
 
 using namespace std;
 
 vector<int> M, P, X;
 int L;
 
+/*
+Function:		bsearch(int, int, int)
+
+Description:	Returns the index j such that lo<=j<=hi, X[M[j]] < X[idx], or 0 if no such index exists. 
+*/
 int bsearch(int idx, int lo, int hi){
 	if(lo >= hi)
 		return X[M[lo]] < X[idx] ? lo : lo-1;
 	return X[M[(lo+hi)/2]] < X[idx] ? bsearch(idx, (lo+hi)/2+1, hi) : bsearch(idx, lo, (lo+hi)/2-1);
 }
 
+/*
+Function:		lis(vector<int>)
+
+Description:	Given a sequence of integers, find a longest increasing subsequence.
+				Returns a vector of indices identifying the first encountered longest increasing subsequence.
+*/
 vector<int> lis(vector<int> seq){
 	vector<int> sol;
 	int len = seq.size();
@@ -27,6 +38,7 @@ vector<int> lis(vector<int> seq){
 	X = seq;
 	M.resize(len+1);
 	P.resize(len+1);
+	//Initialize with first value (needed for bsearch)
 	L = 1;
 	M[0] = P[0] = -1;
 	M[1] = 0;
@@ -42,23 +54,4 @@ vector<int> lis(vector<int> seq){
 	for(int i = M[L], j = L-1; i >= 0; i = P[i], j--)
 		sol[j] = i;
 	return sol;
-}
-
-int main(int argc, char* argv[]){
-	vector<int> seq, sol;
-	int n, tmp, len;
-	while(scanf("%d", &n) > 0){
-		seq.clear();
-		while(n--){
-			scanf("%d", &tmp);
-			seq.push_back(tmp);
-		}
-		sol = lis(seq);
-		len = sol.size();
-		printf("%d\n", len);
-		for(int i = 0; len > 0 && i < len; i++){
-			printf("%d%c", sol[i], i+1 == len ? '\n' : ' ');
-		}
-	}
-	return 0;
 }
---
+++ lib/lis.h
@@ -0,0 +1,15 @@
+/**
+* Filename: lis.h
+* Author:   Markus Niemelä
+*/
+
+#ifndef LIS_H
+#define LIS_H
+
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+std::vector<int> lis(std::vector<int>);
+
+#endif
